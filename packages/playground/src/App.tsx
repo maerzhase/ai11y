3268b11c09750ab0005d25e1a58e61e0deb7be84
maerzhase <markus@lili16.de>
2025-09-27 11:20:19 +0100

    init monorepo

---
+++ packages/playground/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { useRef } from "react";
+
+export default function App() {
+
+  return (
+    <div style={{ minHeight: "200vh", padding: "2rem" }}>
+      <h1>React Quest Playground</h1>
+    </div>
+  );
+}
